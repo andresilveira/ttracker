3e56316bb4fdf7c27835a8133d27a5efbcad1b00
andresilveira <andresilveirah@gmail.com>
2015-10-17 20:05:23 +0200

    fix codeclimate issue with vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,7 @@ require 'vcr'
 require 'minitest-vcr'
 
 VCR.configure do |c|
+  c.ignore_localhost = true
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.ignore_hosts 'codeclimate.com'
