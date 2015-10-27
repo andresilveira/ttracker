f9f23f73b2012981550a0e916c082297b476f51c
andresilveira <andresilveirah@gmail.com>
2015-10-27 11:41:53 +0100

    capitalize item names

--- lib/ttracker/entities/item.rb
+++ lib/ttracker/entities/item.rb
@@ -1,4 +1,18 @@
 class Item
+  class Name
+    def initialize raw_name
+      @raw_name = raw_name.to_s.downcase
+    end
+
+    def to_s
+      @raw_name
+    end
+
+    def capitalize
+      @raw_name.split.map(&:capitalize).join(' ')
+    end
+  end
+
   include Lotus::Entity
   attributes :name
 
@@ -8,5 +22,11 @@ class Item
     @market_entries || []
   end
 
-  attr_writer :market_entries
+  def name=(name)
+    @name = Name.new(name).to_s
+  end
+
+  def name
+    Name.new(@name).capitalize
+  end
 end
