421e05cfc58a415cb9251e5cd4228903dd4d2c49
andresilveira <andresilveirah@gmail.com>
2015-10-27 11:54:36 +0100

    add confirm option to forms

--- apps/web/public/javascripts/application.js
+++ apps/web/public/javascripts/application.js
@@ -1,1 +1,8 @@
-
+$(function() {
+  $('form[data-confirm]').submit(function(e) {
+    confirmMessage = $(this).data('confirm-message') || 'Are you sure?'
+    if(!confirm(confirmMessage)){
+      e.preventDefault();
+    }
+  });
+});
