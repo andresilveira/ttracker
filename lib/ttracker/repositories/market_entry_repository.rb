7dc5f7bee5a9494834b148e5e03f41b701e706ea
andresilveira <andresilveirah@gmail.com>
2015-10-30 11:21:36 +0100

    order market entries in Items#show

--- lib/ttracker/repositories/market_entry_repository.rb
+++ lib/ttracker/repositories/market_entry_repository.rb
@@ -1,7 +1,23 @@
 class MarketEntryRepository
   include Lotus::Repository
 
+  # Gets all the market entries of a given item
+  # @param item_id [Integer] The Item's id
+  # @return [Lotus::Model::Adapters::Sql::Query] the set of market entries for the given item id
   def self.find_by_item_id(item_id)
     query.where(item_id: item_id)
+  end
+
+  # Orders the market entries by its creation date
+  # @param limit [Integer, 20] the optional maximum number of market entries returned
+  # @return [Lotus::Model::Adapters::Sql::Query] the ordered and limmited set of market entriies
+  # @example
+  #   MarketEntryRepository.recently_created(limit: nil)
+  #     => #<Lotus::Model::Adapters::Sql::Query:0x007f9a71a08eb8
+  #       @collection=#<Sequel::Postgres::Dataset: "SELECT * FROM \"market_entries\"">,
+  #       @conditions=[[:order, #<Sequel::SQL::OrderedExpression @expression=>:created_at, @descending=>true, @nulls=>nil>], [:limit, nil]],
+  #       @context=MarketEntryRepository>
+  def self.recently_created(limit: 20)
+    query.desc(:created_at).limit(limit)
   end
 end
--- spec/ttracker/repositories/market_entry_repository_spec.rb
+++ spec/ttracker/repositories/market_entry_repository_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe MarketEntryRepository do
-  # place your tests here
+  before do
+    MarketEntryRepository.clear
+    ItemRepository.clear
+  end
+  let(:item1) { ItemRepository.create(Item.new(name: 'item1')) }
+  let(:item2) { ItemRepository.create(Item.new(name: 'item2')) }
+
+  describe '.find_by_item_id' do
+    it 'should contain only entries from the given item' do
+      entry = MarketEntryRepository.create(MarketEntry.new(price: 1, amount: 1, vendor: 'dude', item_id: item1.id))
+      MarketEntryRepository.find_by_item_id(item1.id).to_a.must_equal [entry]
+    end
+  end
+
+  describe '.recently_created' do
+    before do
+      @new_entry = MarketEntryRepository.create(MarketEntry.new(price: 1, amount: 1, vendor: 'dude', item_id: item1.id, created_at: Date.today))
+      @old_entry = MarketEntryRepository.create(MarketEntry.new(price: 1, amount: 1, vendor: 'dude', item_id: item1.id, created_at: Date.today - 1))
+    end
+
+    it 'should order the entries by its creation date' do
+      MarketEntryRepository.recently_created.to_a.must_equal [@new_entry, @old_entry]
+    end
+
+    it 'should be able to limit the number of entries returned' do
+      MarketEntryRepository.recently_created(limit: 1).to_a.must_equal [@new_entry]
+    end
+  end
 end
